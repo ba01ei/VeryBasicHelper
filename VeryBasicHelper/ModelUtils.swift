def70746c8db155cb5648451351dd5e018406286
Bao Lei <bao@lei.fyi>
2020-11-28 16:49:26 -0800

    made ModelUtils interfaces public

--- VeryBasicHelper/ModelUtils.swift
+++ VeryBasicHelper/ModelUtils.swift
@@ -2,16 +2,16 @@
 //  ModelUtils.swift
 //
 
-protocol DictConvertible {
+public protocol DictConvertible {
     static func from(_ dict: [String: Any]?) -> Self
 }
 
-class ModelUtils {
-    static func value<T>(_ dict: [String: Any]?, key: String, or defaultValue: T) ->  T {
+public class ModelUtils {
+    public static func value<T>(_ dict: [String: Any]?, key: String, or defaultValue: T) ->  T {
         return dict?[key] as? T ?? defaultValue
     }
     
-    static func listValue<T: DictConvertible>(_ dict: [String: Any]?, key: String, or defaultValue: [T] = []) -> [T] {
+    public static func listValue<T: DictConvertible>(_ dict: [String: Any]?, key: String, or defaultValue: [T] = []) -> [T] {
         return (dict?[key] as? [[String: Any]])?.map({T.from($0)}) ?? defaultValue
     }
 }
